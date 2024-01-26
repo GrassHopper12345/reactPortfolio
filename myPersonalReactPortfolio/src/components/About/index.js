8e99f20762a9a03315d4a60e61244ea4ee1ac528
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-25 19:56:36 -0600

    update to about, css, and resume

---
+++ myPersonalReactPortfolio/src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import profilePic from "../../assets/profilePic/myPersonalReactPortfolio/src/assets/profilePic/C9A040E7-5C0C-4033-AA29-BE4741219C99_1_105_c.jpeg"
+
+function About() {
+    return (
+        <section className="my-5 intro" id="about-me">
+            <div class="flex-row">
+                <h1 id="about" className="section-title primary-border">About Me</h1>
+            </div>
+            <div className="flex-row">
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    )
+}
