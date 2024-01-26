665537f582080af52d04acfa37b1c3fa7a552a98
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 08:44:54 -0600

    added helpers and completed about component, started on contact component

---
+++ myPersonalReactPortfolio/src/utills/helpers.js
@@ -0,0 +1,10 @@
+export function capitalizeFirstLetter(str) {
+    if (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+}
+
+export function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
