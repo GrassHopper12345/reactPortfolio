f4a8b48984b652f24e4738b3c301dfdae04adcfa
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 16:05:19 -0600

    added images of completed projects, imported images into portfolio component

---
+++ myPersonalReactPortfolio/src/components/Portfolio/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import cliLogoGen from "../../assets/projectPics/cliLogoGenerator.png";
+import codeQuiz from "../../assets/projectPics/CodeQuiz.png";
+import eCommerce from "../../assets/projectPics/E-Commerce-Back-End.png";
+import fundMyFarm from "../../assets/projectPics/Fund_My_Farm.png";
+import noteTaker from "../../assets/projectPics/NoteTaker.png";
+import pwa from "../../assets/projectPics/PWA-TextEditor.png";
+import readmeGen from "../../assets/projectPics/ReadMEGenerator.png";
+import socialNetwork from "../../assets/projectPics/Social-Network-API.png";
+import weatherApp from "../../assets/projectPics/weatherApp.png";
+import weatherWays from "../../assets/projectPics/WeatherWays.png";
+
+function Portfolio() {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h2></h2>
+                </div>
+            </section>
+        </div>
+    )
+}
