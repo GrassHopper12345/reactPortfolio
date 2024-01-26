0902e7613b8e888e989ec09458b206fc9d61ff4d
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 09:02:02 -0600

    update to contact component

--- myPersonalReactPortfolio/src/components/Contact/index.js
+++ myPersonalReactPortfolio/src/components/Contact/index.js
@@ -1,4 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { validateEmail} from "../../utils/helpers"
 
+    const [email, setEmail] = useState("");
+    const [userName, setUserName] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleInputChange = (e) => {
+        const {target} = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        }else if (inputType === "userName") {
+            setUserName(inputValue);
+        }else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email) || !userName) {
+            setErrorMessage('Please enter a valid Email or Name!');
+            return;
+        }
+
+        if(!setMessage(message)) {
+            setErrorMessage('A message is required!');
+            return;
+        }
+        setUserName("");
+        setEmail("");
+        setMessage("");
+    };
+
+    return (
+        <section>
+            <div></div>
+        </section>
+    )
