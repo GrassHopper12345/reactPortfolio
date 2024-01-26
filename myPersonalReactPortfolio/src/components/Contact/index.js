ce998c4d640c866c67289813740ef72f44825f09
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 10:30:04 -0600

    completed Contact component

--- myPersonalReactPortfolio/src/components/Contact/index.js
+++ myPersonalReactPortfolio/src/components/Contact/index.js
@@ -1,22 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import { validateEmail} from "../../utils/helpers"
+import { validateEmail } from "../../utils/helpers"
 
+function Contact() {
     const [email, setEmail] = useState("");
     const [userName, setUserName] = useState("");
     const [message, setMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
     const handleInputChange = (e) => {
-        const {target} = e;
+        const { target } = e;
         const inputType = target.name;
         const inputValue = target.value;
 
         if (inputType === "email") {
             setEmail(inputValue);
-        }else if (inputType === "userName") {
+        } else if (inputType === "userName") {
             setUserName(inputValue);
-        }else {
+        } else {
             setMessage(inputValue);
         }
     };
@@ -24,12 +25,12 @@ import { validateEmail} from "../../utils/helpers"
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
-        if(!validateEmail(email) || !userName) {
+        if (!validateEmail(email) || !userName) {
             setErrorMessage('Please enter a valid Email or Name!');
             return;
         }
 
-        if(!setMessage(message)) {
+        if (!setMessage(message)) {
             setErrorMessage('A message is required!');
             return;
         }
@@ -39,7 +40,66 @@ import { validateEmail} from "../../utils/helpers"
     };
 
     return (
-        <section>
-            <div></div>
+        <section id="reach-out" className="contact">
+            <div className="flex-row">
+                <h2 className="section-title secondary-border">Reach Out to Connect</h2>
+            </div>
+
+            <div className="cotact-info">
+                <div>
+                    <h3>Hello {userName}</h3>
+                    <p>Would you like to Connect?</p>
+                    <address>
+                        Bossier City, LA <br />
+                        Phone: <a href="tel:318.773.4013">318.773.4013</a>
+                        <br />
+                        Email:{" "}
+                        <a href="mailto://brian.hopper@live.com">brian.hopper@live.com</a>
+                    </address>
+                    <p><strong>I would love to hear from you!</strong></p>
+                </div>
+
+                {/* contact form section */}
+                <div className="contact-form">
+                    <h3>Pleaase React Out!</h3>
+                    <form className="form">
+                        <label for="contact-name">Your Name</label>
+                        <input
+                            value={userName}
+                            name="userName"
+                            onChange={handleInputChange}
+                            type="text"
+                            id="contact-name"
+                            placeholder="Enter your name"
+                        />
+
+                        <label for="contact-email">Your Email</label>
+                        <input
+                            value={email}
+                            name="email"
+                            onChange={handleInputChange}
+                            type="text"
+                            id="contact-email"
+                            placeholder="Enter your email"
+                        />
+
+                        <label for="contact-message">Your Message</label>
+                        <input
+                            value={message}
+                            name="message"
+                            onChange={handleInputChange}
+                            type="text"
+                            id="contact-message"
+                            placeholder="Enter your message"
+                        />
+                        <button type="button" onClick={handleFormSubmit}>Submit</button>
+                    </form>
+                </div>
+                {errorMessage && (
+                    <div><p className="error-text">{errorMessage}</p></div>
+                )}
+            </div>
         </section>
-    )
+    );
+}
+export default Contact;
