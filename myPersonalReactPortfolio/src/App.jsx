fcd4bbe1c5fecb2ffdd470c06a9a5da87e95f924
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 21:50:58 -0600

    update to app.jsx file

--- myPersonalReactPortfolio/src/App.jsx
+++ myPersonalReactPortfolio/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Navi from "./components/Navi";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTab, handleTabChange] = useState("about");
 
-  return (
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Helmet>
+        <title> Brian Hopper's Portfolio | {currentTab} </title>  
+      </Helmet> 
+      ;
+      <Header currentTab={currentTab} handleTabChange={handleTabChange}></Header>
+      <main>{renderTab()}</main>
+      <Footer></Footer>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
