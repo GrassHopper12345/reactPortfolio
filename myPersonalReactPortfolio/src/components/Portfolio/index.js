8d15109f692ae606c915234240b695937eab11ca
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2024-01-26 21:38:55 -0600

    completed resume component

--- myPersonalReactPortfolio/src/components/Portfolio/index.js
+++ myPersonalReactPortfolio/src/components/Portfolio/index.js
@@ -22,17 +22,17 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/logoGenerator">
-                                <img 
-                                src={cliLogoGen}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="CLI-LogoGenerator"/>
+                                <img
+                                    src={cliLogoGen}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="CLI-LogoGenerator" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>CLI-LogoGenerator</h4>
                             <p>
-                            This application enables a user to enter select inputs into inquirer prompts that will generate a logo.svg file that contains the users select input data and save it as an svg file.
+                                This application enables a user to enter select inputs into inquirer prompts that will generate a logo.svg file that contains the users select input data and save it as an svg file.
                             </p>
                         </div>
                     </div>
@@ -43,17 +43,17 @@ function Portfolio() {
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/codeQuiz">
                                 {" "}
-                                <img 
-                                src={codeQuiz}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="CodeQuiz"/>
+                                <img
+                                    src={codeQuiz}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="CodeQuiz" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>CodeQuiz</h4>
                             <p>
-                            AS A student, I wanted a way to test my coding knowledge as well as my fellow piers knowledge about web development. This application contains questions that will test the users knowledge about programming languages. 
+                                AS A student, I wanted a way to test my coding knowledge as well as my fellow piers knowledge about web development. This application contains questions that will test the users knowledge about programming languages.
                             </p>
                         </div>
                     </div>
@@ -63,18 +63,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/ORM-E-Commerce-BE">
-                            {" "}
-                                <img 
-                                src={eCommerce}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="E-Commerce: Back-End"/>
+                                {" "}
+                                <img
+                                    src={eCommerce}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="E-Commerce: Back-End" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>E-Commerce: Back-End</h4>
                             <p>
-                            ORM-E-Commerce-BE is a backend application that providesdatabase creation with the options to add, delete, update data into the application via Insominia. This application utilizes dotenv, mysql2, sequalize, and express frameworks.
+                                ORM-E-Commerce-BE is a backend application that providesdatabase creation with the options to add, delete, update data into the application via Insominia. This application utilizes dotenv, mysql2, sequalize, and express frameworks.
                             </p>
                         </div>
                     </div>
@@ -84,18 +84,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/fund-my-farm">
-                            {" "}
-                                <img 
-                                src={fundMyFarm}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="Fund My Farm"/>
+                                {" "}
+                                <img
+                                    src={fundMyFarm}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="Fund My Farm" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>Fund My Farm</h4>
                             <p>
-                            What is Fund My Farm: E-Commerce? Well we wanted to bring back farms to the market through the internet. We felt that having the ability to see what type of agriculture a farm has just by going to the web could benefit someone looking for the right farm. Also did you know Fund my Farm will allow you to invest right from the website so you will be able to reserve the agriculture that you desire. As a team we learned a lot not just about farms and agriculture but we also learned many new techniques as a full stack development team.
+                                What is Fund My Farm: E-Commerce? Well we wanted to bring back farms to the market through the internet. We felt that having the ability to see what type of agriculture a farm has just by going to the web could benefit someone looking for the right farm. Also did you know Fund my Farm will allow you to invest right from the website so you will be able to reserve the agriculture that you desire. As a team we learned a lot not just about farms and agriculture but we also learned many new techniques as a full stack development team.
                             </p>
                         </div>
                     </div>
@@ -105,18 +105,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/NoteTaker">
-                            {" "}
-                                <img 
-                                src={noteTaker}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="NoteTaker"/>
+                                {" "}
+                                <img
+                                    src={noteTaker}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="NoteTaker" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>NoteTaker</h4>
                             <p>
-                            Note Taker Express is a simple to use personal use note taking application. This application utilizes Node.js and Expresss.js. The express.js runs the backend as well as retrieves from and saves notes to the database as a JSON file.
+                                Note Taker Express is a simple to use personal use note taking application. This application utilizes Node.js and Expresss.js. The express.js runs the backend as well as retrieves from and saves notes to the database as a JSON file.
                             </p>
                         </div>
                     </div>
@@ -126,18 +126,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/progressiveWebApplication-TextEditor">
-                            {" "}
-                                <img 
-                                src={pwa}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="PWA: TextEditor"/>
+                                {" "}
+                                <img
+                                    src={pwa}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="PWA: TextEditor" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>PWA: TextEditor</h4>
                             <p>
-                            This application is a web based web application where a user can create code snippets or notes with or without a connection to the internet. A user can retrieve saved notes and code snippets once connected to the internet. Using the integrated service worker interface and Cache API's, the application will remain functional offline. This application will allow a user to interact with the application while remaining offline.
+                                This application is a web based web application where a user can create code snippets or notes with or without a connection to the internet. A user can retrieve saved notes and code snippets once connected to the internet. Using the integrated service worker interface and Cache API's, the application will remain functional offline. This application will allow a user to interact with the application while remaining offline.
                             </p>
                         </div>
                     </div>
@@ -147,18 +147,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/readMeGenerator">
-                            {" "}
-                                <img 
-                                src={readmeGen}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="Readme Generator"/>
+                                {" "}
+                                <img
+                                    src={readmeGen}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="Readme Generator" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>Readme Generator</h4>
                             <p>
-                            This project is a personal README file generator that prompts its user multiple questions to build a high quality README file.
+                                This project is a personal README file generator that prompts its user multiple questions to build a high quality README file.
                             </p>
                         </div>
                     </div>
@@ -168,18 +168,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/Social-Network-API">
-                            {" "}
-                                <img 
-                                src={socialNetwork}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="Social-Network: API"/>
+                                {" "}
+                                <img
+                                    src={socialNetwork}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="Social-Network: API" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>Social-Network: API</h4>
                             <p>
-                            This API is for a social network application where a user can share their personal or professional thoughts, react to a friends' thought or thoughts, as well as creat a friends list. The web application uses MongoDB for databases, Mongoose for ODM, Express.js for routing, and Moment.js for date & timestamps. The data is seeded using Insomnia.
+                                This API is for a social network application where a user can share their personal or professional thoughts, react to a friends' thought or thoughts, as well as creat a friends list. The web application uses MongoDB for databases, Mongoose for ODM, Express.js for routing, and Moment.js for date & timestamps. The data is seeded using Insomnia.
                             </p>
                         </div>
                     </div>
@@ -189,18 +189,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/weatherApp">
-                            {" "}
-                                <img 
-                                src={weatherApp}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="WeatherApp"/>
+                                {" "}
+                                <img
+                                    src={weatherApp}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="WeatherApp" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>WeatherApp</h4>
                             <p>
-                            This application contains a search input for a user to personally search any given city globally to determine the given destinations weather for 6 days. 6 days of weather include the current day and 5 additional days after. The application also contains quick links for major U.S. cities.
+                                This application contains a search input for a user to personally search any given city globally to determine the given destinations weather for 6 days. 6 days of weather include the current day and 5 additional days after. The application also contains quick links for major U.S. cities.
                             </p>
                         </div>
                     </div>
@@ -210,18 +210,18 @@ function Portfolio() {
                     <div className="job-info">
                         <div className="job-img">
                             <a href="https://github.com/GrassHopper12345/WeatherWays">
-                            {" "}
-                                <img 
-                                src={weatherWays}
-                                className="my-2"
-                                style={{ width: "100%" }}
-                                alt="WeatherWays"/>
+                                {" "}
+                                <img
+                                    src={weatherWays}
+                                    className="my-2"
+                                    style={{ width: "100%" }}
+                                    alt="WeatherWays" />
                             </a>
                         </div>
                         <div className="job-text">
                             <h4>WeatherWays</h4>
                             <p>
-                            WeatherWay is a web application that allows users to input cities for its 7-day forecast and it current traffic. It was built using HTML, CSS, and JavaScript and is designed to be simple and user-friendly.
+                                WeatherWay is a web application that allows users to input cities for its 7-day forecast and it current traffic. It was built using HTML, CSS, and JavaScript and is designed to be simple and user-friendly.
                             </p>
                         </div>
                     </div>
--- myPersonalReactPortfolio/src/components/Resume/index.js
+++ myPersonalReactPortfolio/src/components/Resume/index.js
@@ -1,1 +1,85 @@
 import React from "react";
+import resume from "../../assets/files/myPersonalReactPortfolio/src/assets/files/Brian.Hopper.resume.FullStack.doc";
+import { BsCloudDownload } from "react-icons/bs";
+import { MdFoundation } from "react-icons/md";
+import { GrTechnology } from "react-icons/gr";
+import { GrDocumentPerformance } from "react-icons/gr";
+
+function Resume() {
+    return (
+        <div>
+            <section className="welcome-section" id="download-intro">
+                <div className="flex-row">
+                    <h2 className="section-title primary-border">My Resume</h2>
+                </div>
+                <div className="flex-row">
+                    <div className="download-info">
+                        <div className="download-text">
+                            <a href={resume} className="download-logo" download><BsCloudDownload /></a>
+                            <p>Please check out my Proficiencies!</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="resume-body" id="home-page-body">
+                <div className="article column1">
+                    <p className="column-title">Front-End Frameworks</p>
+                    <p className="column-text">
+                        <ul>
+                            <li className="logo"><MdFoundation /></li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>ES5-ES7</li>
+                            <li>GIT</li>
+                            <li>APIs</li>
+                            <li>Materialize</li>
+                            <li>Bootstrap</li>
+                            <li>Handlebars</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div className="article column2">
+                    <p className="column-title">Back-End Frameworks</p>
+                    <p className="column-text">
+                        <ul>
+                            <li className="logo"><GrTechnology /></li>
+                            <li></li>
+                            <li>Object-Relation--Mapping</li>
+                            <li>Object-Oriented Programming</li>
+                            <li>Sequelize</li>
+                            <li>Express</li>
+                            <li>Jest</li>
+                            <li>Node.Js</li>
+                            <li>Json Web Tokens</li>
+                            <li>Bcrypt</li>
+                            <li>Model-View-Controller</li>
+                            <li>Web-Pack</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div className="article column3">
+                    <p className="column-title">Performance Frameworks</p>
+                    <p className="column-text">
+                        <ul>
+                            <li className="logo"><GrDocumentPerformance /></li>
+                            <li>NoSQL</li>
+                            <li>Mongoose</li>
+                            <li>MongoDB</li>
+                            <li>GraphQL</li>
+                            <li>Apollo</li>
+                            <li>MERN</li>
+                            <li>State</li>
+                            <li>React</li>
+                            <li>Progressive Web Applications</li>
+                        </ul>
+                    </p>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Resume;
